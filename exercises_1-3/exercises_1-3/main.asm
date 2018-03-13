22dded8e09ee9fc5b217cc5eef6e267070d3581d
Patrik I <260010@via.dk>
2018-03-13 11:14:10 +0100

    changes

---
+++ exercises_1-3/exercises_1-3/main.asm
@@ -0,0 +1,12 @@
+;
+; exercises_1-3.asm
+;
+; Created: 13-Mar-18 11:06:07 AM
+; Author : patih
+;
+
+
+
+ldi r16,42			;decimal number into R16
+ldi r17,0x33		;hexadecimal number into r17 (0x<- for hexadecimal)
+ldi r18,0b10110010	;binary number into r18 (0b<-for binary)
---
+++ exercises_1-4/excercise_1-3/main.asm
@@ -0,0 +1,10 @@
+;
+; excercise_1-3.asm
+;
+; Created: 13-Mar-18 11:00:08 AM
+; Author : patih
+;
+
+ldi		r16, 0x08		;adds number 0x08 to register r15
+ldi		r18, 0xb3		;adds number 0xb3 to register r18
+nop
