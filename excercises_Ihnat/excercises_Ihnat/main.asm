4323f825ee31fd452b9240fbbcb0d9057a8d1b7d
Patrik I <260010@via.dk>
2018-03-13 10:14:20 +0100

    Excercises_Ihnat added

---
+++ excercises_Ihnat/excercises_Ihnat/main.asm
@@ -0,0 +1,10 @@
+;
+; excercises_Ihnat.asm
+;
+; Created: 08-Mar-18 11:46:57 AM
+; Author : patih
+;
+
+
+ldi r16, 0b0100_0010    ; load value into register r16
+nop                     ; does nothing, but provides a place to put a breakpoint
