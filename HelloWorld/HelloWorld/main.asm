780afaec70416e7ebf34820d8c7aaf8b83dec783
Patrik I <260010@via.dk>
2018-03-14 10:32:42 +0100

    code modification

--- HelloWorld/HelloWorld/main.asm
+++ HelloWorld/HelloWorld/main.asm
@@ -6,9 +6,11 @@
 ;
 
                             ; SETUP
-    ldi r16, 0xff           ; load the bit pattern 1111 1111 into r16
-    out ddra, r16           ; write the bit pattern to port a's data direction register
+ldi r16, 0xff           ; load the bit pattern 1111 1111 into r16
+out ddra, r16           ; write the bit pattern to port a's data direction register
 
                             ; MAIN
-    ldi r16, ~(1<<pina)     ; invert the bit pattern 0000 0001 and write it to r16
-    out porta, r16          ; write the pattern to port a's output control register
+ldi r16, ~(1<<pina)     ; invert the bit pattern 0000 0001 and write it to r16
+out porta, r16          ; write the pattern to port a's output control register
+
+
