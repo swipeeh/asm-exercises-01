6580836ca9b3874adebf2d7e56c255e52f40f345
Patrik I <260010@via.dk>
2018-03-15 13:49:22 +0100

    christmas lights

--- exercises_1-7/excercises_1-6/main.asm
+++ exercises_1-7/excercises_1-6/main.asm
@@ -8,14 +8,20 @@
 							; SETUP
     ldi r16, 0xff           ; load the bit pattern 1111 1111 into r16
     out ddra, r16           ; write the bit pattern to port a's data direction register
-	ldi r17, 0b0000_0000	;setting led on
-	ldi r18, 0b1111_1111	;setting led off
-	ldi r19, 100
-	ldi r20, 100
-	ldi r21, 100
-	ldi r22, 100
-	ldi r23, 100
-	ldi r24, 100
+	ldi r17, 0b1010_1010	;setting led on
+	ldi r18, 0b0101_0101	;setting led off
+	ldi r19, 255
+	ldi r20, 255
+	ldi r21, 255
+	ldi r22, 255
+	ldi r23, 255
+	ldi r24, 255
+	ldi r25, 255
+	ldi r26, 255
+	ldi r27, 255
+	ldi r28, 255
+	ldi r29, 255
+	ldi r30, 255
 	
 loop:
 	out porta, r17			; putting r17 bit into portA
@@ -30,6 +36,21 @@ delay1:
 		BREQ end5
 		rjmp delay5
 	end5:
+	delay9:
+		DEC r25				; r19--
+		BREQ end9
+		rjmp delay9
+	end9:
+	delay10:
+		DEC r27				; r19--
+		BREQ end10
+		rjmp delay10
+	end10:
+	delay12:
+		DEC r29				; r19--
+		BREQ end12
+		rjmp delay12
+	end12:
 	DEC r19					; r19--
 	BREQ end1
 	rjmp delay1
@@ -48,8 +69,24 @@ delay3:
 		BREQ end6
 		rjmp delay6
 	end6:
+	delay8:
+		DEC r26				; r19--
+		BREQ end8
+		rjmp delay8
+	end8:
+	delay11:
+		DEC r28				; r19--
+		BREQ end11
+		rjmp delay11
+	end11:
+	delay13:
+		DEC r30				; r19--
+		BREQ end13
+		rjmp delay13
+	end13:
 	DEC r22					; r19--
 	BREQ end3
 	rjmp delay3
 end3:
 	rjmp loop
+
