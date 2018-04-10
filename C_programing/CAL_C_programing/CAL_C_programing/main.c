3c93bbea66665a3c52fb99936e91f7093091e260
Patrik I <260010@via.dk>
2018-04-10 08:54:35 +0200

    1 LED blinking

--- C_programing/CAL_C_programing/CAL_C_programing/main.c
+++ C_programing/CAL_C_programing/CAL_C_programing/main.c
@@ -5,12 +5,28 @@
  * Author : patih
  */ 
 
+//defining the cpu oterhwise there will be no delay for half a second, L means long
+#define F_CPU 10000000L
 #include <avr/io.h>
+//this is the import for delays
+#include <util/delay.h>
+//this is parameter 
 #define ALL_ON 0xff
 
 int main(void)
 {
 	DDRA = ALL_ON;
 	PORTA = 0xfe;
+	
+	//loop for blinking 
+	while(1)
+	{
+		PORTA = 0xfe;
+		//delay
+		_delay_ms(500);
+		PORTA = 0xff;
+		//delay	
+		_delay_ms(500);
+	}
 }
 
