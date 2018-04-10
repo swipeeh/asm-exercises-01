182745b7fe6a465513bc2ad52a2e2e8d29330f88
Patrik I <260010@via.dk>
2018-04-10 09:30:38 +0200

    code change main method

--- C_programing/CAL_C_programing/CAL_C_programing/main.c
+++ C_programing/CAL_C_programing/CAL_C_programing/main.c
@@ -5,18 +5,18 @@
  * Author : patih
  */ 
 
-//defining the cpu oterhwise there will be no delay for half a second, L means long
+//defining the cpu otherwise there will be no delay for half a second, L means long
 #define F_CPU 10000000L
 #include <avr/io.h>
 //this is the import for delays
 #include <util/delay.h>
 //this is parameter 
 #define ALL_ON 0xff
-//we can use statements as true and fals
+//we can use statements as true and false
 #include <stdbool.h>
 
-void led_on();
-void led_off();
+void led_on(int led_no);
+void led_off(int led_no);
 
 int main(void)
 {
@@ -26,23 +26,31 @@ int main(void)
 	//loop for blinking 
 	while(1) //we can put there true or 1 
 	{
+		//showing if else statements
+		if(1 == 3)
+		{
+			//do something
+		}
+		else()
+		{
+			
+		}
 		//calling functions 
-		led_on();
-		led_off();
-	}
-	
-	void led_on()
-	{
-		PORTA = 0xfe;
-		_delay_ms(500);
-		return;
-	}
-	
-	void led_off()
-	{
-		PORTA = 0xff;
-		_delay_ms(500);
-		return;
+		led_on(3);
+		led_off(3);
 	}
 }
+	void led_on(int led_no)
+	{
+		PORTA &= ~(1<<led_no); // & ~ this means and inverse
+		_delay_ms(500);
+		return;
+	}
+	
+	void led_off(int led_no)
+	{
+		PORTA = ~(1<<led_no);
+		_delay_ms(500);
+		return;
+	}
 
